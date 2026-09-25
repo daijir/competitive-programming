8def96de1886e3743394e8bc2078b37c91535a76
Daijir <dazhilangxianggen@gmail.com>
2026-09-25 13:53:25 +0900

    third largest number

---
+++ third-largest-number.py
@@ -0,0 +1,31 @@
+# Third Largest Number
+# https://atcoder.jp/contests/abc476/tasks/abc476_c
+
+import sys
+
+def main():
+  input = sys.stdin.read
+  data = input().split()
+  
+  if not data:
+    return
+  
+  N = int(data[0])
+  A = [int(x) for x in data[1:]]
+  
+  top3 = sorted(A[:3], reverse=True)
+  
+  results = []
+  
+  results.append(str(top3[2]))
+  
+  for i in range(3, N):
+    val = A[i]
+    if val > top3[2]:
+      top3[2] = val
+      top3.sort(reverse=True)
+      
+  print("\n".join(results))
+
+if __name__ == "__main__":
+  main()
